7e2352cda190efcc00f96481450feb2967825d93
B <alunocrsul@gmail.com>
2023-05-19 00:59:19 -0300

    19/05/2023

--- Fibonacci.py
+++ Fibonacci.py
@@ -5,7 +5,7 @@ fibonacci = [1,1]
 count = 0
 while (count < num):
     if num > 2:
-        fibonacci.append(fibonacci[count] + fibonacci[count+1]) #criação        
+        fibonacci.append(fibonacci[count] + fibonacci[count+1]) #criação dos termos seguintes   
     count += 1 #incremento do contador
 
 #Retirar termos de acordo com a situação - Precisei usar isso pois o array inicia startado com 2 termos [1, 1] - gambiarra!
